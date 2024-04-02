1e072cef73a54389b2166d7547dbe47190c7bce1
Janet Scanlon <janet.Lscanlon@gmail.com>
2024-04-01 20:10:58 -0500

    onArcane working as expected

--- script.js
+++ script.js
@@ -3,18 +3,35 @@
 // Example:
 // let fungusHP = 100;
 
-function onReady() {
-    console.log("Ready to go!")
+//create state variables: 
+let fungusHP = 100;
+let userAP = 100;
+
+
+//create onAttack functions 
+
+function onArcane(){
+    //update the state variables for the changes after Arcane Attack 
+    fungusHP -= 14;
+    userAP -= 12; 
+}
     
     // Make sure you check the index.html file! 
     // There are lots of buttons and things ready for you to hook into here!
     
     
     // 🧠 Remember
-    // - Handle events that ->
-    // - Updates state which is ->
-    // - Rendered to the DOM
-}
-
-
-onReady()
+    // - Handle events that -> decrease user AP and decrese fungus HP depending on which attack user chose  
+    // - Updates state which is -> Arcane Scepter
+                // AP Cost: 12
+                // HP Damage: 14
+                // entangle Entangle
+                // AP Cost: 23
+                // HP Damage: 9
+                // dragon blade Dragon Blade
+                // AP Cost: 38
+                // HP Damage: 47
+                // star fire Star Fire
+                // AP Cost: 33
+                // HP Damage: 25
+    // - Rendered to the DOM -> updated AP/HP counts and fungus class depending on if the user wins or loses 
